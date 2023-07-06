5930cea53a938511ecbf72a02bd9389493b8c7cc
vivekneupane11 <vivekneupane11@gmail.com>
2023-07-06 19:20:55 +0545

    Navbar init

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,42 @@
-import { useState } from "react"
-
+import { useState } from "react";
+const menuItems = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "Products",
+    path: "/products",
+  },
+  {
+    name: "Our Clients",
+    path: "/clients",
+  },
+  {
+    name: "Blog",
+    path: "/blog",
+  },
+  {
+    name: "About Us",
+    path: "/",
+  },
+  {
+    name: "Contact Us",
+    path: "/",
+  },
+];
 export default function Navbar() {
-    const [toggle,setToggle] = useState(false)
+  const [toggle, setToggle] = useState(false);
   return (
-    <div>
-
-    </div>
-  )
+    <nav className="w-screen-full py-4 px-8">
+      <img src="./assets/logo.jpg" height="60px" width={"60px"} alt="Logo" />
+      <ul>
+        {menuItems.map((item) => (
+          <li>
+            <a href={item.path}> {item.name} </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 }
